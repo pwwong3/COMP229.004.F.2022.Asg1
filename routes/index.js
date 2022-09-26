c9ae64bf56adba1dc31726a675d4a4915b374d26
Wong Pak Wah <pwwong3@users.noreply.github.com>
2022-09-26 15:13:07 -0400

    Include welcome message, Mission Statement and button that allows the user to redirect to your About Me Page

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,11 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: 'WONG Pak Wah | Bulid your own personality' });
+});
+
+router.get('/about', function(req, res, next) {
+  res.render('index', { title: 'About me' });
 });
 
 module.exports = router;
